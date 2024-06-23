5bb55d042c13fd7fa73101963803bb272f16a995
taufik-hdyt <taufikhdyt2599@gmail.com>
2024-06-23 20:40:32 +0700

    add cors

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"go-photo-upload/handlers"
 	"go-photo-upload/utils"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -17,6 +19,15 @@ func main() {
 	}
 
 	router := gin.Default()
+	// Setup CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8080", "https://quotes-server-i1tq.onrender.com"}, // Ganti dengan origin yang sesuai
+		AllowMethods:     []string{"GET", "POST"},
+		AllowHeaders:     []string{"Origin", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	db := utils.SetupDB()
 	handler := handlers.NewHandler(db)
